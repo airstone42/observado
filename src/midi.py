e7143a9da3b959f3f07d40d4cace58fe874aca79
Tianqi <airstone42@gmail.com>
2019-12-23 23:21:35 +0800

    :sparkles: calculate components of midi chords

---
+++ src/midi.py
@@ -0,0 +1,25 @@
+from src.chords import *
+
+
+class MIDITemplate(Template):
+    # MIDI note numbers of C3 to B3, C4 to B4, C5 to B5
+    bass = [x for x in range(48, 59 + 1)]
+    alto = [x for x in range(60, 71 + 1)]
+    treble = [x for x in range(72, 83 + 1)]
+
+    def __init__(self, chord):
+        super().__init__(chord)
+        self.component: list = self._calculate()
+
+    def _calculate(self) -> list:
+        bass = [self.bass[self.chord.bass.value()]]
+        # Pick notes by matching array.
+        alto = [self.alto[x] for x in range(len(self.array)) if self.array[x]]
+        treble = [self.treble[self.chord.bass.value()]]
+        return sorted(bass + alto + treble, reverse=True)
+
+    def __repr__(self):
+        return 'MIDITemplate({!r})'.format(self.__dict__)
+
+    def __str__(self):
+        return 'MIDITemplate({}, {})'.format(str(self.chord), str(self.component))

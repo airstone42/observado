44e0d05cb6c5c6ed0ecfbea9fd8e9040d0c6a322
Tianqi <airstone42@gmail.com>
2019-12-25 07:16:32 +0800

    :sparkles: generate wave from midi files

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import os
+import subprocess
 from concurrent import futures
 
 from src.chords import *
@@ -11,6 +12,15 @@ dirname = os.path.dirname(__file__)
 # Calculate all chords by combine notes and chord patterns.
 all_chords = [(lambda x, y: x + y if y != 'M' else x)(x, y) for x in Note.notes if x not in Note.alt_table.keys()
               for y in Pattern.table.keys()]
+
+
+def dir_check():
+    def check_and_make(subdir):
+        if not os.path.exists(os.path.join(dirname, subdir)):
+            os.mkdir(os.path.join(dirname, subdir))
+
+    for i in ['../data', '../data/midi', '../data/wave']:
+        check_and_make(i)
 
 
 def basic_generate():
@@ -41,9 +51,35 @@ def midi_generate():
         pool.map(generate, all_chords)
 
 
+def wave_generate():
+    def generate(chord):
+        for i in MIDIContent.inst_table:
+            p = MIDIContent(chord, i)
+            filename = p.pattern.chord.notation + '_' + p.inst
+            midi_name = os.path.join(dirname, '../data/midi/{}.mid'.format(filename))
+            wave_name = os.path.join(dirname, '../data/wave/{}.wav'.format(filename))
+            if not os.path.exists(wave_name):
+                # Problems when running on Windows for 'ø7' chord.
+                if 'ø7' in wave_name:
+                    wave_name = wave_name.replace('ø7', 'm7b5')
+                subprocess.run(['timidity', midi_name, '-Ow', '-o', wave_name], stdout=subprocess.DEVNULL)
+
+    with futures.ThreadPoolExecutor(max_workers=12) as pool:
+        pool.map(generate, all_chords)
+
+    # Fix 'ø7' filename after generation.
+    for _, _, files in os.walk(os.path.join(dirname, '../data/wave/')):
+        for file in files:
+            if 'm7b5' in file:
+                os.rename(os.path.join(dirname, '../data/wave/', file),
+                          os.path.join(dirname, '../data/wave/', file.replace('m7b5', 'ø7')))
+
+
 def main():
+    dir_check()
     basic_generate()
     midi_generate()
+    wave_generate()
 
 
 if __name__ == '__main__':

98bda395b26dccc23b5be9ae793e78c41141dce3
Tianqi <airstone42@gmail.com>
2020-01-14 04:41:03 +0800

    :white_check_mark: add test for mean function

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.utils import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_means(self):
+        b = np.array([[x for x in range(0, 18)], [x for x in range(-18, 0)]])
+        a = np.array([3, 9])
+        self.assertTrue(np.array_equal(np.array([[1, 5.5, 12.5], [-17, -12.5, -5.5]]), means(b, a)))
+        self.assertTrue(np.array_equal(np.array([8.5, -9.5]), means(b)))
+
+
+if __name__ == '__main__':
+    unittest.main()

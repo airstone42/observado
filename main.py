247d804ed74b53a74042f9faf9c1139fb3958365
Tianqi <airstone42@gmail.com>
2020-01-27 18:34:04 +0800

    :sparkles: fix problems in main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 import sys
 
 import librosa
+import numpy as np
 
 from src import utils
+from src.chords import Chord
 from src.analyze import analyze
 
 
@@ -12,13 +14,15 @@ def main():
     y, _ = librosa.load(filename)
     b, t = utils.beats(y)
     y = utils.chroma_cens(y)
-    y = utils.means(y, b).transpose()
-    chords = analyze(y)
+    b = np.insert(b, 0, 0)
+    b = np.append(b, y.shape[1] - 1)
+    chords = analyze(y.transpose())
+    chords = [(lambda m: max(set(m), key=m.count))(chords[b[j]:b[j + 1]]) for j in range(len(b) - 1)]
     for i in t:
         print('%.2f' % i, end='    ')
     print()
     for i in chords:
-        print('%s' % i.notation, end='    ')
+        print('%s' % i.notation if isinstance(i, Chord) else i , end='    ')
 
 
 if __name__ == '__main__':
